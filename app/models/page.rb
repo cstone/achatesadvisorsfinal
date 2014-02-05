9de41e4856a82c1994f0937655d85e147f42c6a1
Cavin Stone <cstone2003@hotmail.com>
2014-02-04 21:42:32 -0600

    Add slug to page form

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ActiveRecord::Base
   attr_accessible :content, :slug, :title
 
   validates :slug, uniqueness: true, presence: true,
-            exclusion: {in: %w[signup login]}
+            exclusion: {in: %w[signup login signout]}
 
 end
