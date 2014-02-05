e28cea98eee425051fb9778514937e9394f31b5d
Cavin Stone <cstone2003@hotmail.com>
2014-02-05 08:43:17 -0600

    Disable devise admin registration

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable
-         #:recoverable, #:trackable, #:validatable
+  devise :database_authenticatable,  :rememberable
+         #:recoverable, #:trackable, #:validatable, :registerable,
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
